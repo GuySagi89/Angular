d7bff9eba0d8fbc1f0e1b843bc7826f67ac55992
GuySagi89 <Guysimso@gmail.com>
2021-05-23 15:56:09 +0300

    Calculator added

---
+++ Day 12/ex1-JS/main.js
@@ -0,0 +1,28 @@
+function Calculator(a,b){
+    this.a = a;
+    this.b = b;
+}
+
+Calculator.prototype.add=function(){
+    return this.a + this.b;
+}
+
+Calculator.prototype.sub=function(){
+    return this.a - this.b;
+}
+
+Calculator.prototype.mult=function(){
+    return this.a * this.b;
+}
+
+Calculator.prototype.log=function(){
+    console.log( 'a + b = ' + this.add(this.a,this.b)+'\n'+
+            'a - b = ' + this.sub(this.a,this.b)+'\n'+
+            'a * b = ' + this.mult(this.a,this.b));
+}
+
+var c1 = new Calculator(5,10);
+c1.log();
+
+var c2 = new Calculator(32,12);
+c2.log();
