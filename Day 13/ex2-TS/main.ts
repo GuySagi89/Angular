9d1c2e495f086bdeac3fc3ed6f2932e1836ab662
GuySagi89 <Guysimso@gmail.com>
2021-05-24 21:26:11 +0300

    Added debug support

--- Day 13/ex2-TS/main.ts
+++ Day 13/ex2-TS/main.ts
@@ -3,25 +3,25 @@ import {Exam} from "./exam";
 let exam =new Exam();
 exam.addQuestion({
     caption:"Who is the king of the animals in Africa?",
-    answers:['Marimba','Help, I cant swimba','Earthworm Jimba'],
+    answers:['A. Marimba','B. Help, I cant swimba','C. Earthworm Jimba'],
     correctIndex:0
 });
 
 exam.addQuestion({
     caption:"If Dan was stuck in Mexico City, how many days would it take before he would eat?",
-    answers:['Dan-Food=Time','Mexican food is evil','Mexican food is good; Dan is evil'],
+    answers:['A. Dan-Food=Time','B. Mexican food is evil','C. Mexican food is good; Dan is evil'],
     correctIndex:1
 });
 
 exam.addQuestion({
     caption:"When is Jim's birthday?",
-    answers:['The day he was born','June 9, 1994','Today if you have a present'],
+    answers:['A. The day he was born','B. June 9, 1994','C. Today if you have a present'],
     correctIndex:2
 });
 
 exam.addQuestion({
     caption:"If cigarettes cause cancer, what causes Capricorn?",
-    answers:['Poor-fitting shoes in southern France','Dental floss','One and one half pounds of butter'],
+    answers:['A. Poor-fitting shoes in southern France','B. Dental floss','C. One and one half pounds of butter'],
     correctIndex:2
 });
 
