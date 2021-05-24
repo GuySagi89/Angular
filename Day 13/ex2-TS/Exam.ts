a08b55422a52b4164a3889fb5a307bc57cb01507
GuySagi89 <Guysimso@gmail.com>
2021-05-24 15:30:25 +0300

    ex3

--- Day 13/ex2-TS/Exam.ts
+++ Day 13/ex2-TS/Exam.ts
@@ -2,14 +2,18 @@ import { Question } from "./Question";
 
 class Exam{
     constructor(
+        private questions: Question[]
     ){}
 
     addQuestion(question:Question): void{
-
+        this.questions.push(question);
     }
 
     print(): void{
-
+   
+    for (let question of this.questions) {
+        console.log(question);
+}
     }
 
     grade(answers: number[]): number{
--- Day 13/ex2-TS/bin/Exam.js
+++ Day 13/ex2-TS/bin/Exam.js
@@ -1,11 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var Exam = /** @class */ (function () {
-    function Exam() {
+    function Exam(questions) {
+        this.questions = questions;
     }
     Exam.prototype.addQuestion = function (question) {
+        this.questions.push(question);
     };
     Exam.prototype.print = function () {
+        for (var _i = 0, _a = this.questions; _i < _a.length; _i++) {
+            var question = _a[_i];
+            console.log(question);
+        }
     };
     Exam.prototype.grade = function (answers) {
         return 3;
