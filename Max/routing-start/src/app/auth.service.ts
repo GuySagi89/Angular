06b1b0baafe12e294d25aff1c1f07757c6a305d1
GuySagi89 <guysimso@gmail.com>
2021-06-20 02:58:05 +0300

    routing

---
+++ Max/routing-start/src/app/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { ResolveEnd } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  loggedIn=false;
+
+  isAuthenticated(){
+    const promise = new Promise(
+      (resolve, reject) => {
+        setTimeout(() => {
+          resolve(this.loggedIn);
+        }, 800);
+      }
+    );
+    return promise;
+  }
+
+login(){
+  this.loggedIn=true;
+}
+
+logout(){
+  this.loggedIn=false;
+}
+
+}
---
+++ Max/routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { ServersService } from '../servers.service';
+
+@Component({
+  selector: 'app-edit-server',
+  templateUrl: './edit-server.component.html',
+  styleUrls: ['./edit-server.component.css']
+})
+export class EditServerComponent implements OnInit {
+  server: {id: number, name: string, status: string};
+  serverName = '';
+  serverStatus = '';
+  allowEdit=false;
+
+  constructor(private serversService: ServersService
+              ,private route:ActivatedRoute) { }
+
+  ngOnInit() {
+
+    console.log(this.route.snapshot);
+
+    this.route.queryParams.subscribe((queryParam:Params)=>{
+      this.allowEdit=queryParam['allowEdit']==='1'?true:false;
+    });
+
+    this.server = this.serversService.getServer(+this.route.snapshot.params['id']);
+    this.serverName = this.server.name;
+    this.serverStatus = this.server.status;
+  }
+
+  onUpdateServer() {
+    this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+  }
+
+}
